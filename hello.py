95f5c454f4ce04370a440498a832a96d5184676c
yalaska04 <79913491+yalaska04@users.noreply.github.com>
2021-04-27 20:59:38 +0200

    Fin hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 import pygame as pg 
+import sys 
 
+def fin_juego(): 
+    pg.quit()
+    sys.exit()
+    
 pg.init()
 pantalla = pg.display.set_mode((600, 400)) 
 pg.display.set_caption('Hola')
@@ -9,7 +14,8 @@ game_over = False
 while not game_over:
     # Gestión de eventos 
     for evento in pg.event.get():
-        pass
+        if evento.type == pg.QUIT:
+            game_over = True 
 
     # Gestión del estado
     print('Hola mundo')
@@ -18,5 +24,8 @@ while not game_over:
 
     pantalla.fill((0, 255, 0))
     pg.display.flip()
+
+fin_juego()
+
 
 
