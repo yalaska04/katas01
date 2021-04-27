2d76114963478af13ee7c98635b2757cda9bf8a8
yalaska04 <79913491+yalaska04@users.noreply.github.com>
2021-04-27 23:48:56 +0200

    Bolas que rebotan con diccionario

--- ball.py
+++ ball.py
@@ -1,5 +1,16 @@
 import pygame as pg
-import sys 
+import sys
+from random import randint
+
+def rebotaX(x): 
+    if x <= 0 or x >= ANCHO: 
+        return -1
+    return 1
+
+def rebotaY(y): 
+    if y <= 0 or y >= ALTO: 
+        return -1
+    return 1
 
 ROJO = (255, 0, 0)
 AZUL = (0, 0, 255)
@@ -10,34 +21,52 @@ ALTO = 600
 
 pg.font.init()
 pantalla = pg.display.set_mode((ANCHO,ALTO))
-
-game_over = False
-x = ANCHO // 2
-y = ALTO // 2
-vx = -5
-vy = -5
 reloj = pg.time.Clock() # creamos la instancia Clock
 
+bolas = []
+for _ in range(10): 
+    bola = {'x': randint(0, ANCHO),
+            'y': randint(0, ALTO),
+            'vx': randint(5,10),
+            'vy': randint(5,10),
+            'color': (randint(0,255), randint(0,255), randint(0, 255)),
+    }
+    bolas.append(bola)
+# Bola 1
+x = ANCHO // 2
+y = ALTO // 2
+vx = -7
+vy = -7
+
+# Bola 2
+x2 = randint(0, ANCHO)
+y2 = randint(0, ALTO)
+vx2 = randint(5,15)
+vy2 = randint(5,15)
+
+game_over = False
 while not game_over: 
     reloj.tick(60) # el reloj no permite flujo hasta que llega al tiempo establecido (= tapón temporal)
+
+    # Gestión de eventos
     for evento in pg.event.get(): 
         if evento.type == pg.QUIT: 
             game_over = True 
     
     # Modificación de estado
-    x += vx 
-    y += vy
-  
-    if y <= 0 or y >= ALTO: 
-        vy = -vy
+
+    for bola in bolas: 
+        bola['x'] += bola['vx']
+        bola['y'] += bola['vy']
     
-    if x <= 0 or x >= ANCHO:  
-        vx = -vx
+        bola['vy'] *= rebotaY(bola['y'])
+        bola['vx'] *= rebotaX(bola['x'])
     
     # Gestión de la pantalla
     pantalla.fill(NEGRO) # rellenamos la pantalla
-    pg.draw.circle(pantalla, ROJO, (x, y), 10) # (sup, color, pos, rad)
-
+    for bola in bolas: 
+        pg.draw.circle(pantalla, bola['color'], (bola['x'], bola['y']), 10)# (sup, color, pos, rad)
+        
     pg.display.flip() # refrescar la pantalla 
 
 pg.quit()
