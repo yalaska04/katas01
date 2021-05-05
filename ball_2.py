9cbf8cf62f2452fda5c4bbd5a51b30a5690d7fb8
yalaska04 <79913491+yalaska04@users.noreply.github.com>
2021-05-05 23:45:52 +0200

    controlar velocidad de animación

--- ball_2.py
+++ ball_2.py
@@ -12,16 +12,62 @@ AZUL = (0, 0, 255)
 VERDE = (0, 255, 0)
 NEGRO = (0, 0, 0)
 
-class Marcador(): 
+class Marcador(pg.sprite.Sprite): 
     def __init__(self, x, y, fontsize=25, color=BLANCO): 
+        super().__init__() # llamo al init del sprite. 
         self.fuente = pg.font.SysFont('Arial', fontsize) 
-        self.x = x
-        self.y = y 
+        self.text = '0'
         self.color = color 
+        self.image = self.fuente.render(str(self.text), True, self.color)
+        self.rect = self.image.get_rect(topleft=(x,y))
 
-    def dibuja(self, text, lienzo): 
-        image = self.fuente.render(str(text), True, self.color) # transforma texto en imagen
-        lienzo.blit(image, (self.x, self.y))
+    def update(self, dt): 
+        self.image = self.fuente.render(str(self.text), True, self.color)
+
+class Raqueta(pg.sprite.Sprite):
+    disfraces = ['electric00.png', 'electric01.png', 'electric02.png']
+
+    def __init__(self, x, y, w=100, h=30): 
+        super().__init__()
+        self.imagenes = self.cargaImagenes()
+        self.imagen_actual = 0
+        self.milisegundos_para_cambiar = 1000 // FPS * 5 # cambie disfraz cada 5 fotogramas
+        self.milisegundos_acumulados = 0
+        self.image = self.imagenes[self.imagen_actual]
+        
+        # para que aparezca un rectángulo rojo 
+        # self.image = pg.Surface((w,h), pg.SRCALPHA, 32) # pg.SRCALPHA = sup transparente
+        # pg.draw.rect(self.image, ROJO, pg.Rect(0, 0, w, h), border_radius=5)
+
+        self.rect = self.image.get_rect(centerx = x, bottom = y)
+        self.vx = 7 
+    
+    def cargaImagenes(self): 
+        imagenes = []
+        for fichero in self.disfraces: 
+            imagenes.append(pg.image.load('./images/{}'.format(fichero)))
+        return imagenes
+
+    def update(self, dt): 
+        teclas_pulsadas = pg.key.get_pressed()
+        if teclas_pulsadas[pg.K_LEFT]: 
+            self.rect.x -= self.vx
+
+        if teclas_pulsadas[pg.K_RIGHT]: 
+            self.rect.x += self.vx
+        
+        if self.rect.left <= 0: 
+            self.rect.left = 0 
+        if self.rect.right >= ANCHO: 
+            self.rect.right = ANCHO
+
+        self.milisegundos_acumulados += dt 
+        if self.milisegundos_acumulados >= self.milisegundos_para_cambiar:
+            self.imagen_actual += 1
+            if self.imagen_actual >= len(self.disfraces):
+                self.imagen_actual = 0 
+            self.milisegundos_acumulados = 0
+        self.image = self.imagenes[self.imagen_actual]
 
 class Bola(pg.sprite.Sprite): 
     def __init__(self, x, y): 
@@ -32,7 +78,7 @@ class Bola(pg.sprite.Sprite):
         self.vx = random.randint(5,10) * random.choice([-1,1])
         self.vy = random.randint(5,10) * random.choice([-1,1])
 
-    def update(self): 
+    def update(self, dt): 
         self.rect.x += self.vx
         self.rect.y += self.vy
 
@@ -45,28 +91,40 @@ class Game():
     def __init__(self): 
         self.pantalla = pg.display.set_mode((ANCHO, ALTO))
         self.botes = 0
-        self.cuentaGolpes = Marcador(10,10)
+        self.todoGrupo = pg.sprite.Group() # instancia de grupo 
 
-        self.ballGroup = pg.sprite.Group() # instancia de grupo 
-        for i in range(random.randint(1,20)):
-            bola = Bola(random.randint(0,ANCHO), random.randint(0, ALTO))
-            self.ballGroup.add(bola)
+        self.cuentaSegundos = Marcador(10,10)
+        self.todoGrupo.add(self.cuentaSegundos)
 
+        self.bola = Bola(random.randint(0,ANCHO), random.randint(0, ALTO))
+        self.todoGrupo.add(self.bola)
+
+        self.raqueta = Raqueta(x = ANCHO//2, y = ALTO - 40)
+        self.todoGrupo.add(self.raqueta)
+        
     def bucle_principal(self):
         game_over = False
         reloj = pg.time.Clock()
+        contador_milisegundos = 0 
+        segundero = 0
+
         while not game_over: 
-            reloj.tick(FPS)
+            dt = reloj.tick(FPS)
+            contador_milisegundos += dt 
+
+            if contador_milisegundos >= 1000: 
+                segundero += 1
+                contador_milisegundos = 0 
 
             for evento in pg.event.get():
                 if evento.type == pg.QUIT: 
                     game_over = True
-
-            self.ballGroup.update()
+            
+            self.cuentaSegundos.text = segundero
+            self.todoGrupo.update(dt)
             
             self.pantalla.fill(NEGRO)
-            self.cuentaGolpes.dibuja('Hola', self.pantalla)
-            self.ballGroup.draw(self.pantalla)
+            self.todoGrupo.draw(self.pantalla)
 
             pg.display.flip()
 
